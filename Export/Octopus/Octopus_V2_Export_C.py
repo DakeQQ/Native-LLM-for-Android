a71228ef97cb8d4302dc6ea720e11be310f418e4
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-04-10 19:06:31 +0800

    Update Octopus_V2_Export_C.py

--- Export/Octopus/Octopus_V2_Export_C.py
+++ Export/Octopus/Octopus_V2_Export_C.py
@@ -82,7 +82,7 @@ in_name_C0 = ort_session_C.get_inputs()[0].name
 out_name_C0 = ort_session_C.get_outputs()[0].name
 
 # Pre-process inputs
-prompt = f"{query}"
+prompt = f"Below is the query from the users, please call the correct function and generate the parameters to call the function.\n\nQuery: {query} \n\nResponse:"
 token = tokenizer(prompt, return_tensors='pt')['input_ids']
 ids_len = token.shape[1] + np.zeros(1, dtype=np.int64)
 input_ids = np.zeros((1, max_seq_len), dtype=np.int32)
