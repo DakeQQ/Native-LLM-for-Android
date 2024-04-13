dd61388e0ec2e2d7ef926bc0e98f4941e09a9f62
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-04-13 10:30:27 +0800

    Update project.cpp

--- StableLM_ONNX/StableLM2-1.6B/app/src/main/cpp/project.cpp
+++ StableLM_ONNX/StableLM2-1.6B/app/src/main/cpp/project.cpp
@@ -1,12 +1,5 @@
 #include "project.h"
 
-inline static std::vector<int> get_input_ids(const std::string& query) {
-    std::vector<int> ids = tokenizer->encode(query);
-    ids.insert(ids.begin(),{100277,  882, 198});  // Chat prompt head
-    ids.insert(ids.end(),{100278, 198, 100277, 78191, 198});  // Chat prompt tail
-    return ids;
-}
-
 inline static std::string get_output_words(const int& id) {
     std::string words = tokenizer->decode(id);
     if (words.length() == 6 && words[0] == '<' && words[words.length() - 1] == '>' && words[1] == '0' && words[2] == 'x') {
@@ -76,7 +69,9 @@ Java_com_example_myapplication_MainActivity_Run_1LLM(JNIEnv *env, jclass clazz,
             clear_history();
         }
         const char *query = env->GetStringUTFChars(jquery, nullptr);
-        std::vector<int32_t> get_ids = get_input_ids(query);
+        std::vector<int32_t> get_ids = tokenizer->encode(query);
+        get_ids.insert(get_ids.begin(),{100277,  882, 198});  // Chat prompt head
+        get_ids.insert(get_ids.end(),{100278, 198, 100277, 78191, 198});  // Chat prompt tail
         ids_len = get_ids.size();
         num_ids_per_chat[save_index] = ids_len;
         if (save_index > 0) {
