7ef7d6bdec1c118e1971621ec2697669b8137649
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-04-16 13:50:31 +0800

    Update MiniCPM_Export.py

--- Export/MiniCPM/MiniCPM-1B/MiniCPM_Export.py
+++ Export/MiniCPM/MiniCPM-1B/MiniCPM_Export.py
@@ -37,7 +37,6 @@ past_values_states = past_key_states
 cos_rotary_pos_emb = torch.ones_like(idx_theta)
 cos_rotary_pos_emb = torch.cat((cos_rotary_pos_emb, cos_rotary_pos_emb), dim=-1).unsqueeze(0)
 sin_rotary_pos_emb = cos_rotary_pos_emb
-last_hidden_state = torch.ones((1, max_seq_len, hidden_size), dtype=torch.float32)
 
 print('Export start ...')
 torch.onnx.export(
