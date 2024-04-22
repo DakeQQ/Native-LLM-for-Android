80c6e4c26b7eac020f4cd02aea5d831c35302947
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-04-22 10:22:03 +0800

    Update project.cpp

--- Gemma_ONNX/app/src/main/cpp/project.cpp
+++ Gemma_ONNX/app/src/main/cpp/project.cpp
@@ -14,14 +14,14 @@ inline static void clear_history() {
     attention_mask = -999999999.f;
     accumulate_num_ids[0] = 0;
     num_ids_per_chat[0] = 0;
-    std::fill(input_ids.begin(),input_ids.end(),0);
+    std::fill(input_ids.begin(), input_ids.end(), 0);
 }
 
 extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_example_myapplication_MainActivity_Pre_1Process(JNIEnv *env, jobject clazz) {
     tokenizer.reset(new Sentencepiece);
-    tokenizer->load( vocab_file);
+    tokenizer->load(vocab_file);
     std::vector<float> theta(theta_size, 0.f);
     for (int i = 1; i < theta_size; i++) {
         theta[i] = theta[i - 1] + 2.f;  // even sequence
@@ -31,7 +31,7 @@ Java_com_example_myapplication_MainActivity_Pre_1Process(JNIEnv *env, jobject cl
     }
     theta[0] = 1.f;
     std::vector<float> idx_theta(max_token_history * theta_size, 0.f);
-    std::move(theta.begin(), theta.end(),idx_theta.begin() + theta_size);
+    std::move(theta.begin(), theta.end(), idx_theta.begin() + theta_size);
     int index = theta_size + theta_size;
     for (float i = 2.f; i < static_cast<float> (max_token_history); i += 1.f) {
         for (int j = 0; j < theta.size(); j++) {
