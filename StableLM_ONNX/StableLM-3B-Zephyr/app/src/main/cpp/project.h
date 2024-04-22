c81a5614533ef7c9a9a6f4ae25bf4a93f2e6b8a8
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-04-22 10:16:37 +0800

    Update project.h

--- StableLM_ONNX/StableLM-3B-Zephyr/app/src/main/cpp/project.h
+++ StableLM_ONNX/StableLM-3B-Zephyr/app/src/main/cpp/project.h
@@ -41,17 +41,17 @@ const int end_id_0 = 100257;
 const int end_id_1 = 100278;
 const int past_key_value_size = 40960 * max_token_history;  // 16 * 32 * 80
 const int single_chat_limit = 341; // It is recommended to set it to max_token_history/3, and use phrases like 'go ahead', 'go on', or 'and then?' to continue answering."
+const int theta_size = 10;
 const int next_chat_buffer = max_token_history - single_chat_limit;
 const int input_ids_buffer_size = max_token_history * sizeof(int32_t);
 const int past_key_values_buffer_size = past_key_value_size * sizeof(float);
 const int hidden_state_buffer_size = max_token_history * 2560 * sizeof(float); // hidden_size=2560
 std::vector<int32_t> input_ids(max_token_history, 0);
-std::vector<int> accumulate_num_ids(30,0);  // Just make sure the size is enough before reaching max_token_history.
-std::vector<int> num_ids_per_chat(30,0); // Same size with accumulate_num_ids.
-std::vector<int> save_max_logit_position(max_token_history,0);
-std::vector<float> theta(10, 0.f);
-std::vector<float> cos_rotary_pos_emb(2 * max_token_history * theta.size(),0.f);
-std::vector<float> sin_rotary_pos_emb(cos_rotary_pos_emb.size(),0.f);
+std::vector<int> accumulate_num_ids(30, 0);  // Just make sure the size is enough before reaching max_token_history.
+std::vector<int> num_ids_per_chat(30, 0); // Same size with accumulate_num_ids.
+std::vector<int> save_max_logit_position(max_token_history, 0);
+std::vector<float> cos_rotary_pos_emb(2 * max_token_history * theta.size(), 0.f);
+std::vector<float> sin_rotary_pos_emb(cos_rotary_pos_emb.size(), 0.f);
 const int rotary_pos_emb_buffer_size = cos_rotary_pos_emb.size() * sizeof(float);
 const std::string storage_path = "/storage/emulated/0/Android/data/com.example.myapplication/files/";
 const std::string vocab_file = "/data/user/0/com.example.myapplication/cache/vocab_StableLM_Zephyr.txt";  // We have moved the vocab.txt from assets to the cache folder in Java process.
