8b5e6749b6c75b03e28f26b211d2c0572fb8353b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-06-07 14:47:26 +0800

    Update modeling_minicpm.py

--- Export_ONNX/MiniCPM/MiniCPM-1B/modeling_modified/modeling_minicpm.py
+++ Export_ONNX/MiniCPM/MiniCPM-1B/modeling_modified/modeling_minicpm.py
@@ -1056,7 +1056,7 @@ class MiniCPMForCausalLM(MiniCPMPreTrainedModel):
         hidden_states = self.model.embed_tokens(input_ids[:ids_len]) * self.config.scale_emb
         attention_mask = (1.0 - torch.tril(torch.ones([1, ids_len, kv_seq_len], dtype=torch.float32))) * attention_mask
         for i in range(self.num_layers):
-            hidden_states, self.save_kv_0[i], self.save_kv_1[i] = self.model.layers[i](
+            hidden_states, self.save_key[i], self.save_value[i] = self.model.layers[i](
                 hidden_states=hidden_states,
                 attention_mask=attention_mask,
                 rotary_pos_emb_cos=cos_rotary_pos_emb,
@@ -1067,8 +1067,8 @@ class MiniCPMForCausalLM(MiniCPMPreTrainedModel):
             )
         expand_space = torch.zeros((self.num_layers, self.num_key_value_heads, self.max_seq_len - kv_seq_len, self.head_dim), dtype=torch.float16)
         return (torch.argmax(self.lm_head(self.model.norm(hidden_states[-1, :]) * self.factor)),
-                torch.cat((torch.stack(self.save_kv_0), expand_space), dim=-2),
-                torch.cat((torch.stack(self.save_kv_1), expand_space), dim=-2))
+                torch.cat((torch.stack(self.save_key), expand_space), dim=-2),
+                torch.cat((torch.stack(self.save_value), expand_space), dim=-2))
 
     def prepare_inputs_for_generation(
             self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
