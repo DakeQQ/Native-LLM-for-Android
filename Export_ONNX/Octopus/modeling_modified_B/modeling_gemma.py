9bacb805bf7af2ca4fe37b87197fe1ffa579b071
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-06-14 09:39:56 +0800

    Update modeling_gemma.py

--- Export_ONNX/Octopus/modeling_modified_B/modeling_gemma.py
+++ Export_ONNX/Octopus/modeling_modified_B/modeling_gemma.py
@@ -512,16 +512,16 @@ class GemmaSdpaAttention(GemmaAttention):
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         query_states = self.q_proj(hidden_states).view(ids_len, self.num_heads, self.head_dim).transpose(0, 1)
         key_states = self.k_proj(hidden_states).view(ids_len, self.num_key_value_heads, self.head_dim).transpose(0, 1)
-        key_states = torch.cat((past_key_states.float(), key_states * rotary_pos_emb_cos + rotate_half(key_states) * rotary_pos_emb_sin), dim=-2)
-        value_states = torch.cat((past_value_states.float(), self.v_proj(hidden_states).view(ids_len, self.num_key_value_heads, self.head_dim).transpose(0, 1)), dim=-2)
+        key_states = torch.cat((past_key_states, (key_states * rotary_pos_emb_cos + rotate_half(key_states) * rotary_pos_emb_sin).half()), dim=-2)
+        value_states = torch.cat((past_value_states, self.v_proj(hidden_states).half().view(ids_len, self.num_key_value_heads, self.head_dim).transpose(0, 1)), dim=-2)
         save_key_states = key_states
         save_value_states = value_states
         key_states = repeat_kv(key_states, self.num_key_value_groups, self.num_key_value_heads, self.head_dim)
         value_states = repeat_kv(value_states, self.num_key_value_groups, self.num_key_value_heads, self.head_dim)
         return self.o_proj(torch.matmul(nn.functional.softmax(
             torch.matmul(query_states * rotary_pos_emb_cos + rotate_half(query_states) * rotary_pos_emb_sin,
-                         key_states.transpose(1, 2)) * self.head_dim_factor + attention_mask, dim=-1,
-            dtype=torch.float32), value_states).transpose(0, 1).reshape(ids_len, self.hidden_size).contiguous()), save_key_states.half(), save_value_states.half()
+                         key_states.transpose(1, 2).float()) * self.head_dim_factor + attention_mask, dim=-1,
+            dtype=torch.float32), value_states.float()).transpose(0, 1).reshape(ids_len, self.hidden_size).contiguous()), save_key_states, save_value_states
 
 
 GEMMA_ATTENTION_CLASSES = {
