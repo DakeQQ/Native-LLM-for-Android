e072e65d327256cf89fb83eaabd7a38b0947dacf
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-06-19 09:49:51 +0800

    Update MiniCPM_Export.py

--- Export_ONNX/MiniCPM/MiniCPM-1B/MiniCPM_Export.py
+++ Export_ONNX/MiniCPM/MiniCPM-1B/MiniCPM_Export.py
@@ -97,7 +97,7 @@ out_name_A1 = out_name_A[1].name
 out_name_A2 = out_name_A[2].name
 
 # Pre-process inputs
-prompt = tokenizer.apply_chat_template([{"role": 'user', "content": query}], tokenize=False, add_generation_prompt=False)
+prompt = f"'role': '<user>', 'content': {query}\n'role': '<assistant>', 'content': "
 token = tokenizer(prompt, return_tensors='pt')['input_ids']
 ids_len = token.shape[1] + np.zeros(1, dtype=np.int64)
 input_ids = np.zeros(max_seq_len, dtype=np.int32)
