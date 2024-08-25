e01b4a519cb1591dc0453fe2d1258700fe30ae28
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-08-25 23:21:48 +0800

    Update project.h

--- Gemma_ONNX/app/src/main/cpp/project.h
+++ Gemma_ONNX/app/src/main/cpp/project.h
@@ -6,9 +6,8 @@
 #include "onnxruntime_cxx_api.h"
 #include "nnapi_provider_factory.h"
 #include "tokenizer.hpp"
-#include "half.hpp"
+#include "onnxruntime_float16.h"
 
-using half_float::half;
 const OrtApi* ort_runtime_A;
 OrtSession* session_model_A;
 std::vector<const char*> input_names_A;
@@ -44,7 +43,8 @@ int response_count = 0;
 int save_index = 0;
 int64_t history_len = 0;
 int64_t ids_len = 0;
-half attention_mask = half(-65504.f);
+//Ort::Float16_t attention_mask = Ort::Float16_t(-65504.f);
+float attention_mask = -65504.f;
 const std::string file_name_A = "Gemma_2B_1024_A.ort";
 const std::string file_name_B = "Gemma_2B_1024_B.ort";
 const std::string file_name_C = "Gemma_2B_1024_C.ort";
@@ -52,13 +52,10 @@ const int max_token_history = 1024;  // Please set this value to match the model
 const int hidden_size = 2048;
 const int start_id = 2;
 const int end_id_0 = 1;
+const int end_id_1 = 107;
 const int past_key_value_size = 4608 * max_token_history;  // 18 * 256
 const int single_chat_limit = 341; // It is recommended to set it to max_token_history/3, and use phrases like 'go ahead', 'go on', or 'and then?' to continue answering."
 const int next_chat_buffer = max_token_history - single_chat_limit;
-const int input_ids_buffer_size = max_token_history * sizeof(int32_t);
-const int past_key_values_buffer_size = past_key_value_size * sizeof(half);
-const int hidden_states_B_buffer_size = max_token_history * hidden_size * sizeof(float);
-const int hidden_states_C_buffer_size = hidden_size * sizeof(float);
 std::vector<int32_t> input_ids(max_token_history, 0);
 std::vector<int> accumulate_num_ids(30, 0);  // Just make sure the size is enough before reaching max_token_history.
 std::vector<int> num_ids_per_chat(30, 0); // Same size with accumulate_num_ids.
@@ -67,5 +64,3 @@ const std::string storage_path = "/storage/emulated/0/Android/data/com.example.m
 const std::string vocab_file = "/data/user/0/com.example.myapplication/cache/vocab_Gemma11.txt";  // We have moved the vocab.txt from assets to the cache folder in Java process.
 const char* qnn_htp_so = "/data/user/0/com.example.myapplication/cache/libQnnHtp.so";  //  If use (std::string + "libQnnHtp.so").c_str() instead, it will open failed.
 const char* qnn_cpu_so = "/data/user/0/com.example.myapplication/cache/libQnnCpu.so";  //  If use (std::string + "libQnnCpu.so").c_str() instead, it will open failed.
-void* key_states;
-void* value_states;
