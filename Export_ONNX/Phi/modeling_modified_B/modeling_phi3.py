2f013a7b04edb891b8bc0e08897593cf38f8d32b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-09-10 22:23:20 +0800

    Update modeling_phi3.py

--- Export_ONNX/Phi/modeling_modified_B/modeling_phi3.py
+++ Export_ONNX/Phi/modeling_modified_B/modeling_phi3.py
@@ -1061,6 +1061,7 @@ class Phi3ForCausalLM(Phi3PreTrainedModel):
         self.save_key = [None] * self.num_layers
         self.save_value = [None] * self.num_layers
         self.attention_mask = (1.0 - torch.tril(torch.ones([1, self.max_seq_len, self.max_seq_len], dtype=torch.float32)))
+        self.expand_space_B = torch.zeros((self.num_layers, self.num_key_value_heads, self.max_seq_len, self.head_dim), dtype=torch.float16)
         # Initialize weights and apply final processing
         self.post_init()
 
@@ -1116,10 +1117,10 @@ class Phi3ForCausalLM(Phi3PreTrainedModel):
                 past_value_states=past_value_states[i],
                 ids_len=ids_len
             )
-        expand_space = torch.zeros((self.num_layers, self.num_key_value_heads, self.max_seq_len - kv_seq_len, self.head_dim), dtype=torch.float16)
+        expand_space_B = self.expand_space_B[:, :, kv_seq_len:, :]
         return (torch.argmax(self.lm_head(self.model.norm(hidden_states[-1]))).int(),
-                torch.cat((torch.stack(self.save_key), expand_space), dim=-2),
-                torch.cat((torch.stack(self.save_value), expand_space), dim=-2))
+                torch.cat((torch.stack(self.save_key), expand_space_B), dim=-2),
+                torch.cat((torch.stack(self.save_value), expand_space_B), dim=-2))
 
     # Copied from transformers.models.persimmon.modeling_persimmon.PersimmonForCausalLM.prepare_inputs_for_generation
     def prepare_inputs_for_generation(
