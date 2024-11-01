5cb0e3221bf39c5511d45e352a03e254fb55121c
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-01 16:31:59 +0800

    Update QwenVL_Export_ABC.py

--- Export_ONNX/QwenVL/QwenVL_Export_ABC.py
+++ Export_ONNX/QwenVL/QwenVL_Export_ABC.py
@@ -10,7 +10,7 @@ except:
     HEIGHT_FACTOR = 20                                                                             # Adjust this value to determine the resize shape.
     WIDTH_FACTOR = 36                                                                              # Adjust this value to determine the resize shape.
     IMAGE_RESIZE = [HEIGHT_FACTOR * 28, WIDTH_FACTOR * 28]                                         # 28 = self.patch_size * self.merge_size
-    MAX_SEQ_LENGTH = 1024                                                                          # The max token length. Note, this value include the 10 tokens for system prompt and 720 tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - 730) tokens for query + response.
+    MAX_SEQ_LENGTH = 1024                                                                          # The max token length. Note, this value include the 10 tokens for system prompt and (HEIGHT_FACTOR * WIDTH_FACTOR) tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - (HEIGHT_FACTOR * WIDTH_FACTOR) - 10) tokens for query + response.
 
 
 path = r'/home/dake/Downloads/Qwen2-VL-2B-Instruct/'                                              # Set the folder path where the Qwen2-VL whole project downloaded.
