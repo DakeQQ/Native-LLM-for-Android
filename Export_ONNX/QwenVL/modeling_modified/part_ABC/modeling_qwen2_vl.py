9beefc4ac174c01a10093d7efd30efec4492bf2d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-01 16:38:58 +0800

    Update modeling_qwen2_vl.py

--- Export_ONNX/QwenVL/modeling_modified/part_ABC/modeling_qwen2_vl.py
+++ Export_ONNX/QwenVL/modeling_modified/part_ABC/modeling_qwen2_vl.py
@@ -68,10 +68,10 @@ try:
     from export_config import WIDTH_FACTOR, HEIGHT_FACTOR, IMAGE_RESIZE
 except:
     # Default Values if import failed
-    HEIGHT_FACTOR = 20                                      # Adjust this value to determine the resize shape.
-    WIDTH_FACTOR = 36                                       # Adjust this value to determine the resize shape.
+    HEIGHT_FACTOR = 10                                      # Adjust this value to determine the resize shape.
+    WIDTH_FACTOR = 18                                       # Adjust this value to determine the resize shape.
     IMAGE_RESIZE = [HEIGHT_FACTOR * 28, WIDTH_FACTOR * 28]  # 28 = self.patch_size * self.merge_size
-    MAX_SEQ_LENGTH = 1024                                   # The max token length. Note, this value include the 10 tokens for system prompt and 720 tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - 730) tokens for query + response.
+    MAX_SEQ_LENGTH = 1024                                   # The max token length. Note, this value include the 10 tokens for system prompt and (HEIGHT_FACTOR * WIDTH_FACTOR) tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - (HEIGHT_FACTOR * WIDTH_FACTOR) - 10) tokens for query + response.
 
 
 @dataclass
