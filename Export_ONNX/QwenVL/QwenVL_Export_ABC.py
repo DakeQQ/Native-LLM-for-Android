9d2edc1d2f44f5f757064b5dd92d60751670b80b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-01 16:39:45 +0800

    Update QwenVL_Export_ABC.py

--- Export_ONNX/QwenVL/QwenVL_Export_ABC.py
+++ Export_ONNX/QwenVL/QwenVL_Export_ABC.py
@@ -7,8 +7,8 @@ try:
 except:
     # Default Values if import failed
     INPUT_IMAGE_SIZE = [720, 1280]                                                                 # Input image shape. Should be a multiple of GPU group (e.g., 16) for optimal efficiency.
-    HEIGHT_FACTOR = 20                                                                             # Adjust this value to determine the resize shape.
-    WIDTH_FACTOR = 36                                                                              # Adjust this value to determine the resize shape.
+    HEIGHT_FACTOR = 10                                                                             # Adjust this value to determine the resize shape.
+    WIDTH_FACTOR = 18                                                                              # Adjust this value to determine the resize shape.
     IMAGE_RESIZE = [HEIGHT_FACTOR * 28, WIDTH_FACTOR * 28]                                         # 28 = self.patch_size * self.merge_size
     MAX_SEQ_LENGTH = 1024                                                                          # The max token length. Note, this value include the 10 tokens for system prompt and (HEIGHT_FACTOR * WIDTH_FACTOR) tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - (HEIGHT_FACTOR * WIDTH_FACTOR) - 10) tokens for query + response.
 
