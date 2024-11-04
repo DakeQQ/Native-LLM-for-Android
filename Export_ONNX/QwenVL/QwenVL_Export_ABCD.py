6994c1dab4f90f8c569ed52c40f74364627c0a79
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-04 22:26:40 +0800

    Update QwenVL_Export_ABCD.py

--- Export_ONNX/QwenVL/QwenVL_Export_ABCD.py
+++ Export_ONNX/QwenVL/QwenVL_Export_ABCD.py
@@ -33,26 +33,27 @@ def quantize_to_uint8(tensor, scale, zero_point):
 
 
 class QwenVL_PartB(torch.nn.Module):
-    def __init__(self, embed_data, scale, zero_point, hidden_size, image_token_id):
+    def __init__(self, embed_data, scale, zero_point, hidden_size, max_seq_len):
         super(QwenVL_PartB, self).__init__()
         self.embed_data = embed_data
-        self.scale = scale
-        self.zero_point = zero_point
+        self.scale = scale.half()
+        self.zero_point = zero_point.half()
         self.hidden_size = hidden_size
-        self.image_token_id = image_token_id
+        self.max_seq_len = max_seq_len
 
-    def forward(self, input_ids, kv_seq_len):
-        return self.embed_data[input_ids] * self.scale[input_ids] + self.zero_point[input_ids], torch.zeros((1, 1, kv_seq_len), dtype=torch.float32)
+    def forward(self, input_ids, ids_len):
+        ids = input_ids[:ids_len]
+        hidden_states = self.embed_data[ids] * self.scale[ids] + self.zero_point[ids]
+        return torch.cat((hidden_states, torch.zeros(self.max_seq_len - ids_len, self.hidden_size, dtype=torch.float16)), dim=0)
 
 
 class QwenVL_PartC(torch.nn.Module):
     def __init__(self, max_seq_len):
         super(QwenVL_PartC, self).__init__()
-        self.position_ids = torch.arange(0, max_seq_len, dtype=torch.int16).repeat(3, 1, 1)
-        self.position_ids_2 = torch.ones((3, 1, 1), dtype=torch.float32)
+        self.position_ids = torch.arange(0, max_seq_len, dtype=torch.float16).repeat(1, 3, 1, 1)
 
-    def forward(self, ids_len, kv_seq_len):
-        return self.position_ids[:, :, :ids_len].float(), self.position_ids_2 + ids_len,  (1 - torch.tril(torch.ones([1, ids_len, kv_seq_len], dtype=torch.float32))) * -65504.0
+    def forward(self, dummy):
+        return self.position_ids[dummy]
 
 
 class QwenVL_PartD(torch.nn.Module):
@@ -62,24 +63,22 @@ class QwenVL_PartD(torch.nn.Module):
         self.prompt_head_len = prompt_head_len
         self.max_seq_len = max_seq_len
         self.image_factor_plus = self.image_factor + self.prompt_head_len
-        self.position_ids = torch.arange(0, self.max_seq_len, dtype=torch.int16).repeat(3, 1, 1)
-        self.position_ids_2 = torch.ones((3, 1, 1), dtype=torch.float32) - self.image_factor + width_factor
+        self.position_ids = torch.arange(0, self.max_seq_len, dtype=torch.float16).repeat(3, 1, 1)
         self.position_ids[0, :, self.prompt_head_len: self.image_factor_plus] = self.prompt_head_len
         j = self.prompt_head_len
         for i in range(self.prompt_head_len, self.image_factor_plus, width_factor):
             self.position_ids[1, :, i: i + width_factor] = j
             j += 1
         self.start_id = self.prompt_head_len + width_factor
-        fill_id = torch.arange(self.prompt_head_len, self.start_id, dtype=torch.int16)
+        fill_id = torch.arange(self.prompt_head_len, self.start_id, dtype=torch.float16)
         for i in range(self.start_id, self.image_factor_plus, width_factor):
             self.position_ids[2, :, i: i + width_factor] = fill_id
-        self.fill_tail_position = torch.arange(self.start_id, self.max_seq_len, dtype=torch.int16).repeat(3, 1, 1)
+        self.fill_tail_position = torch.arange(self.start_id, self.max_seq_len, dtype=torch.float16).repeat(3, 1, 1)
 
-    def forward(self, hidden_states, image_embed, ids_len):
-        part_1, part_2 = torch.split(hidden_states, [self.prompt_head_len, hidden_states.shape[0] - self.prompt_head_len], dim=0)
-        position_ids = self.position_ids[:, :, :ids_len]
-        position_ids[:, :, self.image_factor_plus:] = self.fill_tail_position[:, :, :ids_len - self.image_factor_plus]
-        return torch.cat((part_1, image_embed, part_2), dim=0), position_ids.float(), self.position_ids_2 + ids_len
+    def forward(self, hidden_states, image_embed, ids_len, ids_len_minus, split_factor):
+        part_1, part_2, _, part_3 = torch.split(hidden_states, [self.prompt_head_len, ids_len_minus, self.image_factor, split_factor], dim=0)
+        self.position_ids[:, :, self.image_factor_plus:ids_len] = self.fill_tail_position[:, :, :ids_len - self.image_factor_plus]
+        return torch.cat((part_1, image_embed, part_2, part_3), dim=0), self.position_ids
 
 
 # Load the model
@@ -91,19 +90,21 @@ with torch.inference_mode():
     head_dim = model.config.hidden_size // num_heads
     num_layers = model.config.num_hidden_layers
     hidden_size = model.config.hidden_size
-    image_token_id = model.config.image_token_id
 
     prompt_head_len = 5  # \n<|im_start|>user\n<|vision_start|>
     ids_len = torch.tensor([10], dtype=torch.long)  # "10" is just a dummy value.
     history_len = torch.tensor([10], dtype=torch.long)  # "10" is just a dummy value.
     image_embed_size = WIDTH_FACTOR * HEIGHT_FACTOR
-    image_embed = torch.ones((image_embed_size, hidden_size), dtype=torch.float32)
+    image_embed = torch.ones((image_embed_size, hidden_size), dtype=torch.float16)
     pixel_values = torch.ones([1, 3, INPUT_IMAGE_SIZE[0], INPUT_IMAGE_SIZE[1]]).to(torch.float32)
     image_pad_len = torch.tensor([image_embed_size], dtype=torch.long)
     ids_len = ids_len + image_pad_len
     kv_seq_len = ids_len + history_len
-    input_ids = torch.ones(ids_len, dtype=torch.int32)
-    hidden_states = torch.ones((ids_len, hidden_size), dtype=torch.float32)
+    input_ids = torch.ones(max_seq_len, dtype=torch.int32)
+    hidden_states = torch.ones((max_seq_len, hidden_size), dtype=torch.float16)
+    ids_len_minus = torch.tensor(ids_len[0] - prompt_head_len, dtype=torch.int32)
+    split_factor = torch.tensor(max_seq_len - ids_len[0] - image_embed_size, dtype=torch.int32)
+    dummy = torch.tensor(0, dtype=torch.int32)
 
     model.model.embed_tokens.weight.requires_grad = False
     data = model.model.embed_tokens.weight.data
@@ -128,21 +129,16 @@ with torch.inference_mode():
     gc.collect()
     print('\nExport Part_A Done!  \n\nExport Part_B Start...')
 
-    model = QwenVL_PartB(embed_data, scale, zero_point, hidden_size, image_token_id)
+    model = QwenVL_PartB(embed_data, scale, zero_point, hidden_size, max_seq_len)
     torch.onnx.export(
         model,
-        (input_ids, kv_seq_len),
+        (input_ids, ids_len),
         onnx_model_B,
         input_names=[
             'input_ids',
-            'kv_seq_len'
+            'ids_len'
         ],
-        output_names=['hidden_states', 'attention_mask'],
-        dynamic_axes={
-            'input_ids': {0: 'ids_len'},
-            'hidden_states': {0: 'ids_len'},
-            'attention_mask': {2: 'kv_seq_len'}
-        },
+        output_names=['hidden_states'],
         do_constant_folding=True,
         opset_version=17
     )
@@ -158,46 +154,34 @@ with torch.inference_mode():
     model = QwenVL_PartC(max_seq_len)
     torch.onnx.export(
         model,
-        (ids_len, kv_seq_len),
+        (dummy,),
         onnx_model_C,
         input_names=[
-            'ids_len',
-            'kv_seq_len'
+            'dummy'
         ],
-        output_names=['position_ids', 'position_ids_2', 'attention_mask'],
-        dynamic_axes={
-            'position_ids': {2: 'ids_len'},
-            'attention_mask': {1: 'ids_len', 2: 'kv_seq_len'}
-        },
+        output_names=['position_ids'],
         do_constant_folding=True,
         opset_version=17
     )
     del model
-    del kv_seq_len
+    del dummy
     print('\nExport Part_C Done! \n\nExport Part_C Start...')
 
     model = QwenVL_PartD(WIDTH_FACTOR, HEIGHT_FACTOR, prompt_head_len, max_seq_len)
     torch.onnx.export(
         model,
-        (hidden_states, image_embed, ids_len),
+        (hidden_states, image_embed, ids_len, ids_len_minus, split_factor),
         onnx_model_D,
         input_names=[
             'hidden_states',
             'image_embed',
-            'ids_len'
+            'ids_len',
+            'ids_len_minus',
+            'split_factor'
         ],
-        output_names=['hidden_states_out', 'position_ids', 'position_ids_2'],
-        dynamic_axes={
-            'hidden_states': {0: 'ids_len'},
-            'hidden_states_out': {0: 'ids_len'},
-            'position_ids': {2: 'ids_len'}
-        },
+        output_names=['hidden_states', 'position_ids'],
         do_constant_folding=True,
         opset_version=17
     )
-    del model
-    del hidden_states
-    del image_embed
-    gc.collect()
     print('\nExport Part_D Done! \n\nNext, please execute the QwenVL_Export_E.py to export the last part and run the test.')
 
