a83aecccdf99ac19b4fcfcdd8dbbc2fbe89ad1f9
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-15 12:18:45 +0800

    Add files via upload

---
+++ Llama_ONNX/app/src/main/cpp/project.h
@@ -0,0 +1,43 @@
+
+#include <jni.h>
+#include <iostream>
+#include <fstream>
+#include <android/asset_manager_jni.h>
+#include "onnxruntime_cxx_api.h"
+#include "nnapi_provider_factory.h"
+#include "tokenizer.hpp"
+#include "onnxruntime_float16.h"
+
+const OrtApi *ort_runtime_A;
+OrtSession *session_model_A;
+std::vector<const char *> input_names_A;
+std::vector<const char *> output_names_A;
+std::vector<std::vector<std::int64_t>> input_dims_A;
+std::vector<std::vector<std::int64_t>> output_dims_A;
+std::vector<ONNXTensorElementDataType> input_types_A;
+std::vector<ONNXTensorElementDataType> output_types_A;
+std::vector<OrtValue *> input_tensors_A;
+std::vector<OrtValue *> output_tensors_A;
+std::unique_ptr<Tokenizer> tokenizer;
+int response_count = 0;
+int save_index = 0;
+int64_t history_len = 0;
+int64_t ids_len = 0;
+// Ort::Float16_t attention_mask = Ort::Float16_t(-65504.f);
+float attention_mask = -65504.f;
+const std::string file_name_A = "Model_Llama_1B_1024.ort";
+const int max_token_history = 1024; // Please set this value to match the model name flag.
+const int start_id = 128000;
+const int end_id_0 = 128001;
+const int end_id_1 = 128009;
+const int past_key_value_size = 16 * 4 * 64 * max_token_history; // 16 * 4 * 64, Remember edit the value if using others param size model.
+const int single_chat_limit = 341;                        // It is recommended to set it to max_token_history/3, and use phrases like 'go ahead', 'go on', or 'and then?' to continue answering."
+const int next_chat_buffer = max_token_history - single_chat_limit;
+std::vector<int32_t> input_ids(max_token_history, 0);
+std::vector<int> accumulate_num_ids(30, 0); // Just make sure the size is enough before reaching max_token_history.
+std::vector<int> num_ids_per_chat(30, 0);   // Same size with accumulate_num_ids.
+std::vector<int> save_max_logit_position(max_token_history, 0);
+const std::string storage_path = "/storage/emulated/0/Android/data/com.example.myapplication/files/";
+const std::string vocab_file = "/data/user/0/com.example.myapplication/cache/vocab_Llama.txt"; // We have moved the vocab.txt from assets to the cache folder in Java process.
+const char *qnn_htp_so = "/data/user/0/com.example.myapplication/cache/libQnnHtp.so";         //  If use (std::string + "libQnnHtp.so").c_str() instead, it will open failed.
+const char *qnn_cpu_so = "/data/user/0/com.example.myapplication/cache/libQnnCpu.so";         //  If use (std::string + "libQnnCpu.so").c_str() instead, it will open failed.
