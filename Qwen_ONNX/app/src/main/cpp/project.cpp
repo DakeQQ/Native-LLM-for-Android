457f6f9772b6dfb9445cb5b1ea4a516276fb37cd
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-22 16:44:21 +0800

    Update project.cpp

--- Qwen_ONNX/app/src/main/cpp/project.cpp
+++ Qwen_ONNX/app/src/main/cpp/project.cpp
@@ -180,7 +180,9 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1A(JNIEnv *env, jobject
     OrtSessionOptions *session_options_A;
     {
         std::vector<char> fileBuffer;
+        std::vector<char> fileBuffer_external;
         off_t fileSize;
+        off_t fileSize_external;
         if (asset_manager != nullptr)
         {
             AAssetManager *mgr = AAssetManager_fromJava(env, asset_manager);
