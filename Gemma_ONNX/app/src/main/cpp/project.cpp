4aac434687c912f636e3cf933bb88eda372c19e8
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-23 13:27:50 +0800

    Update project.cpp

--- Gemma_ONNX/app/src/main/cpp/project.cpp
+++ Gemma_ONNX/app/src/main/cpp/project.cpp
@@ -244,7 +244,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1A(JNIEnv *env, jobject
                                              "1");  // Use it to lower memory usage.
         ort_runtime_A->AddSessionConfigEntry(session_options_A,
                                              "session.qdq_matmulnbits_accuracy_level",
-                                             "2");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
+                                             "4");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
         ort_runtime_A->AddSessionConfigEntry(session_options_A,
                                              "ep.dynamic.workload_type",
                                              "Default");  // Default = Performance; Efficient = Save Power
@@ -427,7 +427,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1B(JNIEnv *env, jobject
                                              "1");  // Use it to lower memory usage.
         ort_runtime_B->AddSessionConfigEntry(session_options_B,
                                              "session.qdq_matmulnbits_accuracy_level",
-                                             "2");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
+                                             "4");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
         ort_runtime_B->AddSessionConfigEntry(session_options_B,
                                              "ep.dynamic.workload_type",
                                              "Default");  // Default = Performance; Efficient = Save Power
@@ -634,7 +634,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1C(JNIEnv *env, jobject
                                              "1");  // Use it to lower memory usage.
         ort_runtime_C->AddSessionConfigEntry(session_options_C,
                                              "session.qdq_matmulnbits_accuracy_level",
-                                             "2");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
+                                             "4");  // 0:default, 1:FP32, 2:FP16, 3:BF16, 4:INT8
         ort_runtime_C->AddSessionConfigEntry(session_options_C,
                                              "ep.dynamic.workload_type",
                                              "Default");  // Default = Performance; Efficient = Save Power
@@ -650,8 +650,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1C(JNIEnv *env, jobject
             ort_runtime_C->SetIntraOpNumThreads(session_options_C, 1);  // Set to 1.
             ort_runtime_C->SessionOptionsAppendExecutionProvider(session_options_C, "XNNPACK", option_keys.data(), option_values.data(), option_keys.size());
         }
-        status = ort_runtime_C->CreateSessionFromArray(ort_env_C, fileBuffer.data(), fileSize,
-                                                       session_options_C, &session_model_C);
+        status = ort_runtime_C->CreateSessionFromArray(ort_env_C, fileBuffer.data(), fileSize, session_options_C, &session_model_C);
     }
     if (status != nullptr) {
         return JNI_FALSE;
