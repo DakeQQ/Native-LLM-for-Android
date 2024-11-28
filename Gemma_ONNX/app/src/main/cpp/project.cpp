032915a8258c3a4e9c6b90522bdc9a8bb316de80
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-11-28 12:46:17 +0800

    Update project.cpp

--- Gemma_ONNX/app/src/main/cpp/project.cpp
+++ Gemma_ONNX/app/src/main/cpp/project.cpp
@@ -321,7 +321,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1A(JNIEnv *env, jobject
     ort_runtime_A->CreateCpuMemoryInfo(OrtArenaAllocator, OrtMemTypeDefault, &memory_info);
     ort_runtime_A->CreateTensorWithDataAsOrtValue(
             memory_info,
-            reinterpret_cast<void*>(input_ids.data()), max_token_history * sizeof(int32_t),
+            reinterpret_cast<void*>(input_ids.data()), static_cast<size_t> (max_token_history) * sizeof(int32_t),
             input_dims_A[0].data(), input_dims_A[0].size(), input_types_A[0],
             &input_tensors_A[0]);
     ort_runtime_A->CreateTensorWithDataAsOrtValue(
@@ -515,7 +515,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1B(JNIEnv *env, jobject
             input_dims_B[1].data(), input_dims_B[1].size(), input_types_B[1],
             &input_tensors_B[1]);
     std::vector<Ort::Float16_t> past_key_values(past_key_value_size, Ort::Float16_t(0.f));
-    int buffer_size = past_key_value_size * sizeof(Ort::Float16_t);
+    size_t buffer_size = past_key_value_size * sizeof(Ort::Float16_t);
     ort_runtime_B->CreateTensorWithDataAsOrtValue(
             memory_info,
             reinterpret_cast<void *>(past_key_values.data()), buffer_size,
@@ -712,7 +712,7 @@ Java_com_example_myapplication_MainActivity_Load_1Models_1C(JNIEnv *env, jobject
     std::vector<float> hidden_states_C(hidden_size, 0.f);
     ort_runtime_C->CreateTensorWithDataAsOrtValue(
             memory_info,
-            reinterpret_cast<void*>(reinterpret_cast<float*> (hidden_states_C.data())), hidden_size * sizeof(float),
+            reinterpret_cast<void*>(reinterpret_cast<float*> (hidden_states_C.data())), static_cast<size_t> (hidden_size) * sizeof(float),
             input_dims_C[0].data(), input_dims_C[0].size(), input_types_C[0],
             &input_tensors_C[0]);
     ort_runtime_C->ReleaseMemoryInfo(memory_info);
