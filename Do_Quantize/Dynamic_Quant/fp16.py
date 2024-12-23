4895c72ef2db73b8d5d2a03ff79e8054917db840
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2024-12-23 17:26:01 +0800

    Update fp16.py

--- Do_Quantize/Dynamic_Quant/fp16.py
+++ Do_Quantize/Dynamic_Quant/fp16.py
@@ -70,7 +70,7 @@ else:
 # transformers.optimizer, Optional, it may cause errors due to fusion operators.
 # model = optimize_model(quanted_model_path,
 #                        use_gpu=use_gpu,
-#                        opt_level=99,
+#                        opt_level=2,
 #                        num_heads=num_heads,
 #                        hidden_size=hidden_size,
 #                        provider=provider,
