f880d368ed3edd6d617c59f3c5b6c50fba388d77
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-02-07 11:32:45 +0800

    Update project.h

--- Qwen_ONNX/app/src/main/cpp/project.h
+++ Qwen_ONNX/app/src/main/cpp/project.h
@@ -23,21 +23,22 @@ int response_count = 0;
 int save_index = 0;
 int64_t history_len = 0;
 int64_t ids_len = 0;
-// Ort::Float16_t attention_mask = Ort::Float16_t(-65504.f);
-float attention_mask = -65504.f;
-const bool use_deepseek = false;  // Enable if using DeepSeek-Distill-Qwen
+// Ort::Float16_t attention_mask = Ort::Float16_t(-65504.f); // Enable if using all fp16 format
+float attention_mask = -65504.f;                             // Disable if using all fp16 format
+
+const bool use_deepseek = false;                             // Enable if using DeepSeek-Distill-Qwen
 const std::string file_name_A = "Model_Qwen_1_5B_1024.ort";
-const std::string file_name_A_external = "NONE";   // If using external data to load the model, provide the file name; otherwise, set to "NONE". If contains many parts, please modify the project.cpp line 313-315.
-const int max_token_history = 1024; // Please set this value to match the model name flag.
+const std::string file_name_A_external = "NONE";             // If using external data to load the model, provide the file name; otherwise, set to "NONE". If contains many parts, please modify the project.cpp line 313-315.
+const int max_token_history = 1024;                          // Please set this value to match the model name flag.
 const int start_id = 151644;
 const int end_id_0 = 151643;
 const int end_id_1 = 151645;
 const size_t past_key_value_size = 7168 * max_token_history; // 28 * 2 * 128, Remember edit the value if using others param size model.
-const int single_chat_limit = 341;                        // It is recommended to set it to max_token_history/3, and use phrases like 'go ahead', 'go on', or 'and then?' to continue answering."
+const int single_chat_limit = 341;                        
 const int next_chat_buffer = max_token_history - single_chat_limit;
 std::vector<int32_t> input_ids(max_token_history, 0);
-std::vector<int> accumulate_num_ids(30, 0); // Just make sure the size is enough before reaching max_token_history.
-std::vector<int> num_ids_per_chat(30, 0);   // Same size with accumulate_num_ids.
+std::vector<int> accumulate_num_ids(30, 0);                  // Just make sure the size is enough before reaching max_token_history.
+std::vector<int> num_ids_per_chat(30, 0);                    // Same size with accumulate_num_ids.
 std::vector<int> save_max_logit_position(max_token_history, 0);
 const std::string storage_path = "/storage/emulated/0/Android/data/com.example.myapplication/";
 const std::string vocab_file = "/data/user/0/com.example.myapplication/cache/vocab_Qwen.txt"; // We have moved the vocab.txt from assets to the cache folder in Java process.
