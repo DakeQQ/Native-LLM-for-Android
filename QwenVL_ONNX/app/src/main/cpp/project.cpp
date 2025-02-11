52280c22c263525525c784e93d802200683e06e7
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-02-11 16:26:11 +0800

    Update project.cpp

--- QwenVL_ONNX/app/src/main/cpp/project.cpp
+++ QwenVL_ONNX/app/src/main/cpp/project.cpp
@@ -1,17 +1,67 @@
 #include "project.h"
 
+inline static void correctUtfBytes(char* bytes) {
+    char three = 0;
+    while (*bytes != '\0') {
+        unsigned char utf8 = *(bytes++);
+        three = 0;
+        // Switch on the high four bits.
+        switch (utf8 >> 4) {
+            case 0x00:
+            case 0x01:
+            case 0x02:
+            case 0x03:
+            case 0x04:
+            case 0x05:
+            case 0x06:
+            case 0x07:
+                // Bit pattern 0xxx. No need for any extra bytes.
+                break;
+            case 0x08:
+            case 0x09:
+            case 0x0a:
+            case 0x0b:
+            case 0x0f:
+                /*
+                 * Bit pattern 10xx or 1111, which are illegal start bytes.
+                 * Note: 1111 is valid for normal UTF-8, but not the
+                 * modified UTF-8 used here.
+                 */
+                *(bytes-1) = '?';
+                break;
+            case 0x0e:
+                // Bit pattern 1110, so there are two additional bytes.
+                utf8 = *(bytes++);
+                if ((utf8 & 0xc0) != 0x80) {
+                    --bytes;
+                    *(bytes-1) = '?';
+                    break;
+                }
+                three = 1;
+                // Fall through to take care of the final byte.
+            case 0x0c:
+            case 0x0d:
+                // Bit pattern 110x, so there is one additional byte.
+                utf8 = *(bytes++);
+                if ((utf8 & 0xc0) != 0x80) {
+                    --bytes;
+                    if(three)--bytes;
+                    *(bytes-1)='?';
+                }
+                break;
+        }
+    }
+}
 
 inline static std::string get_output_words(const int &id)
 {
     std::string words = tokenizer->decode(id);
-    if (words.empty())
-    {
-        return "?";
-    }
     if (words.length() == 6 && words[0] == '<' && words[words.length() - 1] == '>' && words[1] == '0' && words[2] == 'x')
     {
         words = static_cast<char>(std::stoi(words.substr(3, 2), nullptr, 16));
+
     }
+    correctUtfBytes(words.data());
     return words;
 }
 
