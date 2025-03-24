6dd5940b8f99d5592f9fe8bb44bf259fab9ef4ba
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-24 12:55:16 +0800

    Update modeling_llama.py

--- Export_ONNX/Llama/modeling_modified/modeling_llama.py
+++ Export_ONNX/Llama/modeling_modified/modeling_llama.py
@@ -830,8 +830,8 @@ class LlamaForCausalLM(LlamaPreTrainedModel, GenerationMixin):
         self.head_dim = self.hidden_size // self.num_heads
         self.save_key = [None] * self.num_layers
         self.save_value = [None] * self.num_layers
-        self.attention_mask = (1 - torch.tril(torch.ones([1, self.max_seq_len, self.max_seq_len], dtype=torch.uint8)))
-        self.expand_space = torch.zeros((self.num_layers, self.num_key_value_heads, self.max_seq_len, self.head_dim), dtype=torch.uint8)
+        self.attention_mask = (1 - torch.tril(torch.ones([1, self.max_seq_len, self.max_seq_len], dtype=torch.int8)))
+        self.expand_space = torch.zeros((self.num_layers, self.num_key_value_heads, self.max_seq_len, self.head_dim), dtype=torch.int8)
         # Initialize weights and apply final processing
         self.post_init()
 
