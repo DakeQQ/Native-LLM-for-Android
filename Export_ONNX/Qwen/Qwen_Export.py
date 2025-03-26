abc259509f7c4aec970e0dc0bc98afdc64319557
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-26 10:11:56 +0800

    Update Qwen_Export.py

--- Export_ONNX/Qwen/Qwen_Export.py
+++ Export_ONNX/Qwen/Qwen_Export.py
@@ -153,11 +153,11 @@ while history_len < max_single_chat_length:
     if (token_id == 151643) | (token_id == 151645):  # the stop_id in Qwen is "151643" & "151645"
         break
     else:
-        history_len[0] += ids_len[0]
-        ids_len[0] = 1
+        history_len += ids_len
+        if num_decode < 1:
+            ids_len = np.array([1], dtype=np.int64)
+            attention_mask = np.array([0.0], dtype=np.float32)
         num_decode += 1
-        attention_mask[0] = 0.0
         input_ids[0] = token_id
         print(tokenizer.decode(token_id), end="", flush=True)
-end_time = time.time()
-print(f"\n\nDecode: {(num_decode / (end_time - start_time)):.3f} token/s")
+print(f"\n\nDecode: {(num_decode / (time.time() - start_time)):.3f} token/s")
