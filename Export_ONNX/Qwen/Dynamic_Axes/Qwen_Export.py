eb447b403eeece423cba7a3769b4f71ee3d85243
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-29 12:35:46 +0800

    Update Qwen_Export.py

--- Export_ONNX/Qwen/Dynamic_Axes/Qwen_Export.py
+++ Export_ONNX/Qwen/Dynamic_Axes/Qwen_Export.py
@@ -23,7 +23,7 @@ hidden_size = model.config.hidden_size
 # Generate dummies for torch.onnx.export()
 attention_mask = torch.tensor([-65504.0], dtype=torch.float32)
 input_ids = torch.ones((1, 10), dtype=torch.int32)  # "10" is just a dummy value.
-past_key_states = torch.zeros((num_layers, num_key_value_heads, 0, head_dim), dtype=torch.float16)
+past_key_states = torch.zeros((num_key_value_heads, 0, head_dim), dtype=torch.float16)
 past_values_states = past_key_states
 position_ids = torch.zeros((max_seq_len, 1), dtype=torch.float32)
 for i in range(max_seq_len):
@@ -61,28 +61,43 @@ del scale
 del zero_point
 gc.collect()
 
+# Prepare input and output names
+input_names = ['input_ids', 'attention_mask']
+output_names = ['max_logit_id']
+dynamic_axes = {'input_ids': {1: 'ids_len'}}
+keys_values = []
+for i in range(num_layers):
+    key_name = f'in_key_{i}'
+    input_names.append(key_name)
+    keys_values.append(past_key_states)
+    dynamic_axes[key_name] = {1: 'history_len'}
+    key_name = f'out_key_{i}'
+    output_names.append(key_name)
+    dynamic_axes[key_name] = {1: 'history_len_plus_ids_len'}
+
+for i in range(num_layers):
+    value_name = f'in_values_{i}'
+    input_names.append(value_name)
+    keys_values.append(past_values_states)
+    dynamic_axes[value_name] = {1: 'history_len'}
+    value_name = f'out_values_{i}'
+    output_names.append(value_name)
+    dynamic_axes[value_name] = {1: 'history_len_plus_ids_len'}
+
+
 print('Export start ...')
 with torch.inference_mode():
+    input_feed = [input_ids, attention_mask] + keys_values
     torch.onnx.export(
-        model, (
-            input_ids, attention_mask, past_key_states, past_values_states),
+        model,
+        tuple(input_feed),
         onnx_model_A,
-        input_names=[
-            'input_ids',
-            'attention_mask',
-            'in_past_key_states',
-            'in_past_values_states'
-        ],
-        output_names=['max_logit_id', 'out_past_key_states', 'out_past_values_states'],
-        dynamic_axes={
-            'input_ids': {1: 'ids_len'},
-            'in_past_key_states': {2: 'history_len'},
-            'in_past_values_states': {2: 'history_len'},
-            'out_past_key_states': {2: 'history_len_plus_ids_len'},
-            'out_past_values_states': {2: 'history_len_plus_ids_len'}
-        },
+        input_names=input_names,
+        output_names=output_names,
+        dynamic_axes=dynamic_axes,
         do_constant_folding=True,
-        opset_version=17)
+        opset_version=17
+    )
 del model
 del input_ids
 del attention_mask
@@ -98,10 +113,10 @@ tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
 
 # ONNX Runtime settings
 session_opts = onnxruntime.SessionOptions()
-session_opts.log_severity_level = 3  # error level, it a adjustable value.
-session_opts.inter_op_num_threads = 0  # Run different nodes with num_threads. Set 0 for auto.
-session_opts.intra_op_num_threads = 0  # Under the node, execute the operators with num_threads. Set 0 for auto.
-session_opts.enable_cpu_mem_arena = True  # True for execute speed; False for less memory usage.
+session_opts.log_severity_level = 4         # fatal level, it an adjustable value.
+session_opts.inter_op_num_threads = 0       # Run different nodes with num_threads. Set 0 for auto.
+session_opts.intra_op_num_threads = 0       # Under the node, execute the operators with num_threads. Set 0 for auto.
+session_opts.enable_cpu_mem_arena = True    # True for execute speed; False for less memory usage.
 session_opts.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
 session_opts.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
 session_opts.add_session_config_entry("session.intra_op.allow_spinning", "1")
@@ -110,13 +125,6 @@ session_opts.add_session_config_entry("session.inter_op.allow_spinning", "1")
 ort_session_A = onnxruntime.InferenceSession(onnx_model_A, sess_options=session_opts, providers=['CPUExecutionProvider'])
 in_name_A = ort_session_A.get_inputs()
 out_name_A = ort_session_A.get_outputs()
-in_name_A0 = in_name_A[0].name
-in_name_A1 = in_name_A[1].name
-in_name_A2 = in_name_A[2].name
-in_name_A3 = in_name_A[3].name
-out_name_A0 = out_name_A[0].name
-out_name_A1 = out_name_A[1].name
-out_name_A2 = out_name_A[2].name
 
 # Pre-process inputs
 if "Deep" in path or "deep" in path or "Distill" in path or "distill" in path:
@@ -130,27 +138,37 @@ else:
     token = tokenizer(prompt, return_tensors='pt')['input_ids']
 input_ids = token.int().numpy()
 attention_mask = np.array([-65504.0], dtype=np.float32)
-past_key_states_A = np.zeros((num_layers, num_key_value_heads, 0, head_dim), dtype=np.float16)
+past_key_states_A = np.zeros((num_key_value_heads, 0, head_dim), dtype=np.float16)
 past_values_states_A = past_key_states_A
 num_decode = 0
 print('\n\nTest Question: ' + query + "\nQwen Answering:\n")
 
+output_names = []
+input_feed = {
+    in_name_A[0].name: input_ids,
+    in_name_A[1].name: attention_mask,
+}
+for i in range(2, len(in_name_A)):
+    input_feed[in_name_A[i].name] = past_key_states_A
+for i in range(len(out_name_A)):
+    output_names.append(out_name_A[i].name)
+
 # Start to run LLM
 start_time = time.time()
 while num_decode < max_single_chat_length:
-    input_ids, past_key_states_A, past_values_states_A = ort_session_A.run(
-        [out_name_A0, out_name_A1, out_name_A2],
-        {
-            in_name_A0: input_ids,
-            in_name_A1: attention_mask,
-            in_name_A2: past_key_states_A,
-            in_name_A3: past_values_states_A
-        })
-    if input_ids in [151643, 151645]:  # the stop_id in Qwen is "151643" & "151645"
+    max_ids, *keys_values = ort_session_A.run(
+        output_names,
+        input_feed
+    )
+    if max_ids in [151643, 151645]:  # the stop_id in Qwen is "151643" & "151645"
         break
     else:
+        input_feed[in_name_A[0].name] = max_ids
+        for i in range(2, len(in_name_A)):
+            input_feed[in_name_A[i].name] = keys_values[i - 2]
         if num_decode < 1:
-            attention_mask = np.array([0.0], dtype=np.float32)
+            input_feed[in_name_A[1].name] = np.array([0.0], dtype=np.float32)
         num_decode += 1
-        print(tokenizer.decode(input_ids[0]), end="", flush=True)
+        print(tokenizer.decode(max_ids[0]), end="", flush=True)
 print(f"\n\nDecode: {(num_decode / (time.time() - start_time)):.3f} token/s")
+
