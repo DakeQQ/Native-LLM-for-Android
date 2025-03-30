7774341725733e9b93c2a00313c871f5a0ac15fe
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-30 16:47:02 +0800

    Update fp16.py

--- Do_Quantize/Dynamic_Quant/fp16.py
+++ Do_Quantize/Dynamic_Quant/fp16.py
@@ -20,6 +20,7 @@ download_path = r'C:\Users\Downloads\Qwen2-1.5B-Instruct'                       
 use_gpu = True                                                                   # If true, the transformers.optimizer will remain the FP16 processes.
 provider = 'CPUExecutionProvider'                                                # ['CPUExecutionProvider', 'CUDAExecutionProvider'']
 use_low_memory_mode_in_Android = False                                           # If you need to use low memory mode on Android, please set it to True.
+upgrade_opset = 21                                                               # optional
 
 
 # Convert the fp32 to fp16
@@ -107,7 +108,7 @@ slim(
 # Upgrade the Opset version. (optional process)
 try:
     model = onnx.load(quanted_model_path)
-    model = onnx.version_converter.convert_version(model, 21)
+    model = onnx.version_converter.convert_version(model, upgrade_opset)
     onnx.save(model, quanted_model_path, save_as_external_data=is_large_model)
     del model
     gc.collect()
