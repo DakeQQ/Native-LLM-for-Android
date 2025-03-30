59840825b85d0be3083cd519fab2c7aa5ad343fa
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-03-30 16:35:50 +0800

    Update q8_f16.py

--- Do_Quantize/Dynamic_Quant/q8_f16.py
+++ Do_Quantize/Dynamic_Quant/q8_f16.py
@@ -122,6 +122,8 @@ try:
     model = onnx.load(quanted_model_path)
     model = onnx.version_converter.convert_version(model, 21)
     onnx.save(model, quanted_model_path, save_as_external_data=is_large_model)
+    del model
+    gc.collect()
 except FileNotFoundError:
     print(f"Error: The model file at {quanted_model_path} was not found.")
 except onnx.checker.ValidationError as e:
