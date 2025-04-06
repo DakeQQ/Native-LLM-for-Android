b066c6cd8ee04462144fc439fdccf342a12f231b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-06 11:24:47 +0800

    Delete Export_ONNX/InternVL/Mono/export_config.py

--- Export_ONNX/InternVL/Mono/export_config.py
+++
@@ -1,3 +0,0 @@
-INPUT_IMAGE_SIZE = [960, 960]       # Input image shape. Should be a multiple of GPU group (e.g., 16) for optimal efficiency.
-MAX_SEQ_LENGTH = 1024               # The max token length. Note, this value include the 10 tokens for system prompt and (HEIGHT_FACTOR * WIDTH_FACTOR) tokens for image prompt. Hence, only (MAX_SEQ_LENGTH - (HEIGHT_FACTOR * WIDTH_FACTOR) - 10) tokens for query + response.
-PROMPT_HEAD_LENGTH = 5              # <s><|im_start|>user\n<img>
