024a4a3fd92b5bc6b1f420c0c7d11f19ef9f8bd4
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-04-06 17:04:26 +0800

    Update fp16.py

--- Do_Quantize/Dynamic_Quant/fp16.py
+++ Do_Quantize/Dynamic_Quant/fp16.py
@@ -55,26 +55,30 @@ slim(
 )
 
 
-if download_path == "NONE":
-    num_heads = 0    # default
-    hidden_size = 0  # default
-else:
-    if ('vl' in download_path.lower()) & ('qwen' in download_path.lower()):
-        if "2.5" in download_path or "3b" in download_path.lower():
-            from transformers import Qwen2_5_VLForConditionalGeneration
-            model = Qwen2_5_VLForConditionalGeneration.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
-        else:
-            from transformers import Qwen2VLForConditionalGeneration
-            model = Qwen2VLForConditionalGeneration.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
-    else:
-        model = AutoModelForCausalLM.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
-    num_heads = model.config.num_attention_heads
-    hidden_size = model.config.hidden_size
-    del model
-    gc.collect()
-
-
 # transformers.optimizer, Optional, it may cause errors due to fusion operators.
+# if download_path == "NONE":
+#     num_heads = 0    # default
+#     hidden_size = 0  # default
+# else:
+#     if ('vl' in download_path.lower()) & ('qwen' in download_path.lower()):
+#         if "2.5" in download_path or "3b" in download_path.lower():
+#             from transformers import Qwen2_5_VLForConditionalGeneration
+#             model = Qwen2_5_VLForConditionalGeneration.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
+#         else:
+#             from transformers import Qwen2VLForConditionalGeneration
+#             model = Qwen2VLForConditionalGeneration.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
+#     else:
+#         model = AutoModelForCausalLM.from_pretrained(download_path, torch_dtype=torch.float16, device_map='cpu', trust_remote_code=True, low_cpu_mem_usage=True).eval()
+#     try:
+#         num_heads = model.config.num_attention_heads
+#         hidden_size = model.config.hidden_size
+#     except:
+#         num_heads = model.config.llm_config.num_attention_heads
+#         hidden_size = model.config.llm_config.hidden_size
+#     del model
+#     gc.collect()
+
+
 # model = optimize_model(quanted_model_path,
 #                        use_gpu=use_gpu,
 #                        opt_level=2,
