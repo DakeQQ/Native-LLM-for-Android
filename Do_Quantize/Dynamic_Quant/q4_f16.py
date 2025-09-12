75d1f811ac7c3f4f537cddbfef1d18f3cb1cce56
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-12 10:54:15 +0800

    Update q4_f16.py

--- Do_Quantize/Dynamic_Quant/q4_f16.py
+++ Do_Quantize/Dynamic_Quant/q4_f16.py
@@ -167,7 +167,7 @@ gc.collect()
 slim(
     model=quanted_model_path,
     output_model=quanted_model_path,
-    no_shape_infer=False,           # False for more optimize but may get errors.
+    no_shape_infer=True,           # False for more optimize but may get errors.
     skip_fusion_patterns=False,
     no_constant_folding=False,
     save_as_external_data=is_large_model,
