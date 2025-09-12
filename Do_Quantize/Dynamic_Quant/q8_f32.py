628f9febecb758088b4f6e2bc16a5cd30ca88fc3
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-09-12 11:03:38 +0800

    Update q8_f32.py

--- Do_Quantize/Dynamic_Quant/q8_f32.py
+++ Do_Quantize/Dynamic_Quant/q8_f32.py
@@ -113,7 +113,7 @@ gc.collect()
 slim(
     model=quanted_model_path,
     output_model=quanted_model_path,
-    no_shape_infer=False,                                     # False for more optimize but may get errors.
+    no_shape_infer=True,                                     # False for more optimize but may get errors.
     skip_fusion_patterns=False,
     no_constant_folding=False,
     save_as_external_data=is_large_model,
@@ -157,3 +157,4 @@ if not is_large_model:
     target_platform = "arm"                 # ['arm', 'amd64']; The 'amd64' means x86_64 desktop, not means the AMD chip.
     # Call subprocess may get permission failed on Windows system.
     subprocess.run([f'python -m onnxruntime.tools.convert_onnx_models_to_ort --output_dir {quanted_folder_path} --optimization_style {optimization_style} --target_platform {target_platform} --enable_type_reduction {quanted_folder_path}'], shell=True)
+
