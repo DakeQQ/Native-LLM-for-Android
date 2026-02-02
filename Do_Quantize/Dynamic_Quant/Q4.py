3518ccfed398653402ac5d6b356000e2994029a0
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2026-02-02 11:51:49 +0800

    Update Q4.py

--- Do_Quantize/Dynamic_Quant/Q4.py
+++ Do_Quantize/Dynamic_Quant/Q4.py
@@ -62,7 +62,7 @@ UPGRADE_OPSET = 0               # Target ONNX Opset version (0 = keep current ve
 def get_model_paths(model_name):
     """Determines source and destination paths for a given model."""
     # Special handling for LLM_Main path location
-    if 'LLM_Main' in model_name:
+    if ('LLM_Main' in model_name) and ('vl' in DOWNLOAD_PATH.lower()):
         src_dir = ORIGINAL_FOLDER_PATH.replace('Qwen_ONNX', 'Qwen_ONNX_2')
     else:
         src_dir = ORIGINAL_FOLDER_PATH
@@ -342,3 +342,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
