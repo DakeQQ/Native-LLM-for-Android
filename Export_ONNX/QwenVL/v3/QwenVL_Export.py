3953bd4ea8e5d4e4498f3ce58a1a6f4e816d365d
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2026-02-13 00:00:52 +0800

    Update QwenVL_Export.py

--- Export_ONNX/QwenVL/v3/QwenVL_Export.py
+++ Export_ONNX/QwenVL/v3/QwenVL_Export.py
@@ -93,11 +93,11 @@ class GREEDY_SEARCH(torch.nn.Module):
 
 
 class FIRST_BEAM_SEARCH(torch.nn.Module):
-    def __init__(self, num_layers):
+    def __init__(self, total_layers):
         super(FIRST_BEAM_SEARCH, self).__init__()
-        self.num_keys_values = num_layers
-        self.save_keys_values = [None] * self.num_keys_values
-        self.batch_indices = torch.arange(MAX_BEAM_SIZE, dtype=torch.int8)
+        self.total_layers = total_layers
+        self.save_keys_values = [None] * self.total_layers
+        self.batch_indices = torch.arange(MAX_BEAM_SIZE, dtype=torch.int64)
 
     def forward(self, *all_inputs):
         logits = all_inputs[-5]
@@ -106,11 +106,11 @@ class FIRST_BEAM_SEARCH(torch.nn.Module):
         penality_value = all_inputs[-2]
         beam_size = all_inputs[-1]
         logits = torch.log_softmax(logits, dim=-1)
-        top_beam_prob, top_beam_indices = torch.topk(logits, dim=-1, k=beam_size, sorted=False, largest=True)
-        for i in range(self.num_keys_values):
+        top_beam_prob, top_beam_indices = torch.topk(logits, dim=-1, k=beam_size, sorted=True, largest=True)
+        for i in range(self.total_layers):
             self.save_keys_values[i] = all_inputs[i].repeat(beam_size, *([1] * (all_inputs[i].dim() - 1)))
         top_beam_indices = top_beam_indices.transpose(0, 1)
-        batch_indices = self.batch_indices[:beam_size].long()
+        batch_indices = self.batch_indices[:beam_size]
         repeat_penality[batch_indices, top_beam_indices] *= penality_value
         top_beam_indices = top_beam_indices.int()
         save_id = torch.cat([save_id, top_beam_indices], dim=-1)
@@ -119,11 +119,10 @@ class FIRST_BEAM_SEARCH(torch.nn.Module):
 
 
 class SECOND_BEAM_SEARCH(torch.nn.Module):
-    def __init__(self, num_layers):
+    def __init__(self, total_layers):
         super(SECOND_BEAM_SEARCH, self).__init__()
-        self.num_keys_values = num_layers
-        self.save_keys_values = [None] * self.num_keys_values
-        self.batch_indices = torch.arange(MAX_BEAM_SIZE, dtype=torch.int8)
+        self.total_layers = total_layers
+        self.save_keys_values = [None] * self.total_layers
 
     def forward(self, *all_inputs):
         logits = all_inputs[-8]
@@ -135,12 +134,12 @@ class SECOND_BEAM_SEARCH(torch.nn.Module):
         beam_size = all_inputs[-2]
         topK = all_inputs[-1]
         logits = torch.log_softmax(logits * repeat_penality, dim=-1)
-        top_k_prob, top_k_indices = torch.topk(logits, k=topK, dim=-1, largest=True, sorted=False)
+        top_k_prob, top_k_indices = torch.topk(logits, k=topK, dim=-1, largest=True, sorted=True)
         current_prob = (top_k_prob + previous_prob).view(-1)
-        top_beam_prob, top_beam_indices = torch.topk(current_prob, k=beam_size, dim=-1, largest=True, sorted=False)
+        top_beam_prob, top_beam_indices = torch.topk(current_prob, k=beam_size, dim=-1, largest=True, sorted=True)
         beam_index = top_beam_indices // topK
         top_beam_indices = top_k_indices.view(-1)[top_beam_indices]
-        for i in range(self.num_keys_values):
+        for i in range(self.total_layers):
             self.save_keys_values[i] = all_inputs[i][beam_index]
         repeat_penality = repeat_penality[beam_index]
         repeat_penality[batch_indices, top_beam_indices] *= penality_value
@@ -1460,3 +1459,4 @@ else:
 print(f"\n\nFinal:\n{result}\n\nDecode: {tokens_per_second:.3f} token/s")
 print(f"Total tokens generated: {num_decode}")
 print(f"Total time: {elapsed_time:.3f}s")
+
